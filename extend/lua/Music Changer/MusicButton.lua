b335e12271cdf7645369a9d55624c1df76fb2415
world power <107804942@qq.com>
2026-01-24 11:13:55 +0800

    new

--- extend/lua/Music Changer/MusicButton.lua
+++ extend/lua/Music Changer/MusicButton.lua
@@ -10,17 +10,17 @@ Controls.OK:RegisterCallback(Mouse.eRClick, function()
 end);
 
 
-Controls.CLEAR:RegisterCallback(Mouse.eLClick, function() 
-	for i = 0, GameDefines.MAX_CIV_PLAYERS- 1 do
+--Controls.CLEAR:RegisterCallback(Mouse.eLClick, function() 
+	--for i = 0, GameDefines.MAX_CIV_PLAYERS- 1 do
 
-	      if   Players[i]:IsAlive()
-		  and  Players[i]~= Players[Game.GetActivePlayer()]   then 
-		  Players[i]:KillUnits();
-		  end
-		  end
-end);
+	     -- if   Players[i]:IsAlive()
+		 -- and  Players[i]~= Players[Game.GetActivePlayer()]   then 
+		  --Players[i]:KillUnits();
+		 -- end
+		 -- end
+--end);
 
-Controls.CLEAR:RegisterCallback(Mouse.eRClick, function() 
-	local select = Controls.Select
-	select:SetHide(false)
-end);
+--Controls.CLEAR:RegisterCallback(Mouse.eRClick, function() 
+	--local select = Controls.Select
+	--select:SetHide(false)
+--end);
